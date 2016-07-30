41ee98f5175a6b2ad2a7dbab4aa577508dfbdcbb
shoheiyokoyama <shohei.yok0602@gmail.com>
2016-07-30 13:56:32 +0900

    crete UIColorExtension.swift

--- Source/AnimatableComponent.swift
+++ Source/AnimatableComponent.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 struct AnimationDefaultColor {
-    static let border     = UIColor(red: 54/255, green: 215/255, blue: 183/255, alpha: 1)
-    static let background = UIColor(red: 248/255, green: 148/255, blue: 6/255, alpha: 1)
-    static let text       = UIColor(red: 214/255, green: 69/255, blue: 65/255, alpha: 1)
-    static let ripple     = UIColor(red: 75/255, green: 119/255, blue: 190/255, alpha: 1)
+    static let border     = UIColor(hex: 0x36D7B7)
+    static let background = UIColor(hex: 0xF89406)
+    static let text       = UIColor(hex: 0xD64541)
+    static let ripple     = UIColor(hex: 0x4B77BE)
 }
 
 protocol AnimatableComponent {
